8a0623a733655ab244cd93ddf8ac3f4a6f1a34f2
nilesh200309 <nkhairnar543@gmail.com>
2023-10-20 00:04:24 +0530

    wrong Program

---
+++ prime.js
@@ -0,0 +1,27 @@
+function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+    if (num <= 3) {
+      return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+      return false;
+    }
+  
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  const number = 17; // Change this to the number you want to check for primality
+  if (isPrime(number)) {
+    console.log(`${number} is a prime number.`);
+  } else {
+    console.log(`${number} is not a prime number.`);
+  }
+  
